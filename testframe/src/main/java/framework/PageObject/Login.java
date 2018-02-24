24c81ef6e89c9fedcb8328997eacf8e2b76e811a
admin <raviukolte@gmail.com>
2018-02-24 21:21:39 +0530

    T

--- testframe/src/main/java/framework/PageObject/Login.java
+++ testframe/src/main/java/framework/PageObject/Login.java
@@ -1,9 +1,6 @@
 package framework.PageObject;
 
-import java.io.IOException;
-
 import org.apache.log4j.Logger;
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
