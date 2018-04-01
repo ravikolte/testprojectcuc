1cf03692a604969ae2bee60daf93632705ba052e
admin <raviukolte@gmail.com>
2018-04-01 10:45:42 +0530

    Added Extent reporter

--- testframe/src/test/java/com/testframe/runner/Login/Loginrunner.java
+++ testframe/src/test/java/com/testframe/runner/Login/Loginrunner.java
@@ -1,5 +1,11 @@
 package com.testframe.runner.Login;
 
+import java.util.Properties;
+
+import org.testng.annotations.AfterClass;
+
+import com.cucumber.listener.Reporter;
+
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
@@ -11,7 +17,14 @@ import cucumber.api.testng.AbstractTestNGCucumberTests;
 },       
         glue = {
 		"classpath:com.testframe.stepdefinition",
-		"classpath:framework.helper" }, plugin = {"html:target/cucumber-html-report"})
+		"classpath:framework.helper" }, plugin = {"html:target/cucumber-html-report","com.cucumber.listener.ExtentCucumberFormatter:C:\\Users\\Ravi\\testprojectcuc\\git\\testframe\\Reports\\report.html"})
 public class Loginrunner extends AbstractTestNGCucumberTests {
 
+	@AfterClass
+	 public static void reportSetup()
+	 {
+		Reporter.loadXMLConfig("C:\\Users\\Ravi\\testprojectcuc\\git\\testframe\\extent-config.xml");
+		Properties p =System.getProperties();
+		p.list(System.out);
+	 }
 }
